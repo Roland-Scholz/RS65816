fe1f5830d2a1c6a29b4b6b80c033f06735688f50
Roland Scholz <rolandscholz@gmx.de>
2026-09-10 17:01:11 +0200

    wdc_clib

---
+++ src/wdc_clib/gensym.cmd
@@ -0,0 +1,14 @@
+path=%PATH%;C:\github\atari-tools\WDC\bin
+
+wdcobj -r  amisc.oc		 > amisc.txt	
+wdcobj -r  astdlib.oc      > astdlib.txt 
+wdcobj -r  astring.oc      > astring.txt 
+wdcobj -r  csup.oc         > csup.txt    
+wdcobj -r  data.oc         > data.txt    
+wdcobj -r  farsbrk.oc      > farsbrk.txt 
+wdcobj -r  misc.oc         > misc.txt    
+wdcobj -r  sbrk.oc         > sbrk.txt    
+wdcobj -r  stdio.oc        > stdio.txt   
+wdcobj -r  stdlib.oc       > stdlib.txt
+wdcobj -r  string.oc       > string.txt  
+wdcobj -r  time.oc         > time.txt    
